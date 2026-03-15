865b10b205c452e741c71e7e44e08d5032986696
luisparadinha <luis.m.paradinha@gmail.com>
2026-03-15 23:43:12 +0000

    final

--- src/random_agent.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-class random_agent:
-    def __init__(self, num_actions):
-        self.num_actions = num_actions
-        
-    def act(self, state):
-        return np.random.choice(self.num_actions)
